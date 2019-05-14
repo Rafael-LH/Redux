30c88f5d2acddef1b78a4a046cd79f721b65cdd2
Rafael Lopez <rafael.iskoat@gmail.com>
2019-05-13 21:28:44 -0500

    Imprimiendo datos del estado

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,4 +1,5 @@
 import './redux.css'
+import {createStore} from 'redux'
 
 
 document.getElementById('form').addEventListener('submit', handleSubmit)
@@ -9,5 +10,44 @@ function handleSubmit(e){
 
         let data = new FormData(document.getElementById('form') )
         let title = data.get('title')
+
         /* console.log(title) */
     }
+
+    const initialState = [
+        {
+            "title": "Despacito"
+        },
+        {
+            "title": "On more time"
+        },
+        {
+            "title": "echame la culpa"
+        }
+    ]
+
+   let store = createStore(
+        state => state,
+        initialState,
+        // extension para window redux (solo poner en el desarrollo)
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+
+    let container = document.getElementById('playlist')
+    let playlist = store.getState()
+
+    playlist.forEach(items => {
+        
+           let element = document.createElement('p')
+           let text = document.createTextNode(items.title)
+           
+            // tambien se puede hacer de esta manera
+            // element.textContent = items.title
+           
+           element.appendChild(text)
+           container.appendChild(element)
+
+    });
+
+    //obtenemos los datos de mi store
+    // console.log(store.getState() )
