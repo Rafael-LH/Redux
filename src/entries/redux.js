72d3b510cfc7818c523130e3ae9d40abf7e1d088
Rafael Lopez <rafael.iskoat@gmail.com>
2019-05-14 21:57:43 -0500

    Actualizando el Store con Reducers

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -11,7 +11,21 @@ function handleSubmit(e){
         let data = new FormData(document.getElementById('form') )
         let title = data.get('title')
 
-        /* console.log(title) */
+        // Esto es una accion y una accion es un bloque de informacion que envia datos a la aplicacion
+       store.dispatch({
+            // lo unico obligatorio es que el key tiene que tener el nombre de type y le tenemos que dar un valor
+            // algo que sea de preferencia significativo con lo que estamos haciendo
+            type: 'ADD_SONG', //como convencion se pone el valor en mayuscula y espacios con _
+            playload: { //aqui va la data en este caso le puse como nombre payload pero le puedo dar el nombre que yo quiera
+                title // con ecma 6 si el nombre de mi key es igual que el nombre de mi valor solo dejamos una vez no hace falta poner title:title
+
+            }
+        })
+
+        // Las acciones se utilizan con el método dispatch()
+        // Son la única fuente de información del store. Solamente de esa forma el store puede saber que los states cambian.
+        // Son objetos planos
+
     }
 
     const initialState = [
@@ -26,12 +40,28 @@ function handleSubmit(e){
         }
     ]
 
+    const reducer = function(state, action){
+            switch (action.type) {
+                case 'ADD_SONG':
+                    return [
+                        // spread operator
+                        ...state,
+                        action.playload //action.playload es un objeto que esta en el dispatch
+                    ]    
+                    break;
+                
+                default:
+                    return state;
+                    break;
+            }
+    }
+
 //    create store recibe tres parametros
 //    1- el state
 //    2- state inicial
 //    3- extension para el browser para hacer debug, esto ultimo solo se tiene que poner en modo de produccion  
    let store = createStore(
-        state => state,
+        reducer,
         initialState,
         // extension para window redux (solo poner en el desarrollo)
         // link para la extensio https://github.com/zalmoxisus/redux-devtools-extension
@@ -56,3 +86,27 @@ function handleSubmit(e){
 
     //obtenemos los datos de mi store
     // console.log(store.getState() )
+
+
+// NOTA MUY IMPORTANTE
+// Reducer
+
+// Modifican nuestro estado
+// Puede haber múltiples reducers en la aplicación y un solo store.
+// Devuelve el siguiente estado.
+// Que no deben hacer
+
+// Modificar los argumentos recibidos
+// Llamar a APIs (u otras tareas secundarias)
+// Llamar a funciones no puras como Date.now(), Math.random()
+// <h1>Funciones puras</h1>
+// Es un concepto de programación funcional, hace que el código sea más legible. Tienen las siguientes condiciones:
+// Dados los mismos datos de entrada, deben retornar el mismo resultado sin importar el número de veces que se llame.
+// No debe tener objetos secundarios.
+// El reducer se iguala a una función, esta función recibe dos parámetros que son el state y la action y en la función
+// se debe definir que se hace con state y action; generalmente se valida con un switch.
+
+// ¿Qué es una función pura?
+
+// “Dados los mismos parámetros/argumentos/entradas deben retornar el mismo resultado, sin importar el número de veces que se llame”
+
